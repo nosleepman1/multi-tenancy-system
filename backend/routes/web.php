d68338a35e5717abac5b5527fb9552f23a3ea454
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-06-02 02:53:02 +0000

    feat: integrate stancl/tenancy for multi-tenant support with database and domain migrations

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// routes/web.php, api.php or any other central route files you have
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // your actual routes
+        
+        Route::get('/test', function () {
+            return "Hello World";
+        });
+
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+}
+
+
+
+
